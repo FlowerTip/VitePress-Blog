a64088ddf52a8fd7b89fd1745772406cdc738396
aven <17516019587@163.com>
2025-06-23 11:49:44 +0800

    feat: 添加 Twikoo 评论组件并更新主题布局

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,8 @@
-import { h } from 'vue'
+import { h } from 'vue';
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Confetti from "./components/Confetti.vue";
+import Twikoo from './components/Twikoo.vue';
 import './style.css'
 
 export default {
@@ -9,10 +10,12 @@ export default {
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'home-features-after': () => h(Confetti)
+      'home-features-after': () => h(Confetti),
+      'doc-after': () => h(Twikoo)
     })
   },
   enhanceApp({ app, router, siteData }) {
     app.component("Confetti", Confetti); //注册全局组件
+    app.component("Twikoo", Twikoo); //注册全局组件
   }
-} satisfies Theme
+} satisfies Theme;
