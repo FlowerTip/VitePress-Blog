830231c9e91c943edc878bbf45ad674ea6f6e8c0
aven <17516019587@163.com>
2025-06-23 13:14:42 +0800

    删除 Twikoo 组件的引用和注册

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,6 @@ import { h } from 'vue';
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Confetti from "./components/Confetti.vue";
-import Twikoo from './components/Twikoo.vue';
 import './style.css'
 
 export default {
@@ -11,11 +10,9 @@ export default {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
       'home-features-after': () => h(Confetti),
-      'doc-after': () => h(Twikoo)
     })
   },
   enhanceApp({ app, router, siteData }) {
     app.component("Confetti", Confetti); //注册全局组件
-    app.component("Twikoo", Twikoo); //注册全局组件
   }
 } satisfies Theme;
